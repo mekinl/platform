8ddbd74318bfacbbc95daa6a81c45bff6ce07d36
Mekin <mekinl@cmu.edu>
2015-02-14 12:06:40 -0500

    Almost finished with the Authentication Part

--- framework/classes/seeds/StripeSeed.php
+++ framework/classes/seeds/StripeSeed.php
@@ -6,9 +6,10 @@
 class StripeSeed extends SeedBase {
 	protected $api_username, $api_password, $api_signature, $api_endpoint, $api_version, $paypal_base_url, $error_message, $token;
 	protected $merchant_email = false;
-
+	//Have to fix constructor based on what the charge method need
 	public function __construct($user_id, $connection_id, $token=false) {
 		$this->settings_type = 'com.mailchimp';
+		error_log("********##Constructor called");
 		$this->user_id = $user_id;
 		$this->connection_id = $connection_id;
 		if ($this->getCASHConnection()) {
@@ -45,9 +46,11 @@ class StripeSeed extends SeedBase {
 	public static function getRedirectMarkup($data=false) {
 		$connections = CASHSystem::getSystemSettings('system_connections');
 		if (isset($connections['com.stripe'])) {
-			error_log($connections['com.stripe']."********");
-			//will have to change login_url
-			$login_url = "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_5eCOhyxL07uaKmLYp44UPuAWzrPx1CKi";
+			//while (list($key, $value) = each($connections['com.stripe'])) {
+			// error_log("Key: $key; Value: $value");
+			//}
+			//$login_url = "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_5eCOhyxL07uaKmLYp44UPuAWzrPx1CKi";
+			$login_url = StripeSeed::getAuthorizationUrl($connections['com.stripe']['client_id'],$connections['com.stripe']['client_secret']);
 			$return_markup = '<h4>Stripe</h4>'
 						   . '<p>This will redirect you to a secure login at Stripe and bring you right back.</p>'
 						   . '<a href="' . $login_url . '" class="button">Connect your Stripe</a>';
@@ -59,10 +62,60 @@ class StripeSeed extends SeedBase {
 	public static function handleRedirectReturn($data=false) {
 		if (isset($data['code'])) {
 			$connections = CASHSystem::getSystemSettings('system_connections');
+			/*
+			foreach ($data as &$value) {
+				error_log($value);
+			}
+			*/
+			// will be moved to method or new classes
 			if (isset($connections['com.stripe'])) {
-				return 'Test successful will have to implement :(';
+			 error_log($data['code']."*****)))");
+			/* 
+			$token_request_body = array(
+				'grant_type' => 'authorization_code',
+				'client_id' => 'ca_5eCOhyxL07uaKmLYp44UPuAWzrPx1CKi',
+				'code' => $data['code'],
+				'client_secret' => 'sk_test_Q8qTx3blDe9wIfORkxLFIAHb'
+			);
+
+			 $req = curl_init('https://connect.stripe.com/oauth/token');
+			 curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+			 curl_setopt($req, CURLOPT_POST, true );
+			 curl_setopt($req, CURLOPT_POSTFIELDS, http_build_query($token_request_body));
+									
+			// TODO: Additional error handling
+			  $respCode = curl_getinfo($req, CURLINFO_HTTP_CODE);
+			  $resp = json_decode(curl_exec($req), true);
+			  curl_close($req);
+			*/
+			$credentials = StripeSeed::exchangeCode($data['code'],
+								$connections['com.stripe']['client_id'],
+								$connections['com.stripe']['client_secret']);
+			//error_log("*******".$resp[access_token]);
+			//foreach ($resp as &$value) {
+			//	error_log($value);
+			//}
+			
+				if (isset($credentials['refresh'])) {
+					$user_info = StripeSeed::getUserInfo($credentials['access']);
+					$new_connection = new CASHConnection(AdminHelper::getPersistentData('cash_effective_user'));
+					$result = $new_connection->setSettings(
+						$user_info['email'] . ' (Stripe)',
+						'com.stripe',
+						array(
+							'access_token'   => $credentials['access']
+						)
+					);
+				if ($result) {
+					AdminHelper::formSuccess('Success. Connection added. You\'ll see it in your list of connections.','/settings/connections/');
+				} else {
+					AdminHelper::formFailure('Error. Could not save connection.','/settings/connections/');
+				}
+				}else{
+					return 'Could not find a refresh token from Stripe';
+				}
 			} else {
-				return 'Please add default google drive app credentials.';
+				return 'Please add default stripe app credentials.';
 			}
 		} else {
 			return 'There was an error. (session) Please try again.';
@@ -77,181 +130,40 @@ class StripeSeed extends SeedBase {
 		return $this->error_message;
 	}
 	
-	protected function postToPaypal($method_name, $nvp_parameters) {
-		// Set the API operation, version, and API signature in the request.
-		$request_parameters = array (
-			'METHOD'    => $method_name,
-			'VERSION'   => $this->api_version,
-			'PWD'       => $this->api_password,
-			'USER'      => $this->api_username,
-			'SIGNATURE' => $this->api_signature
-		);
-		if ($this->merchant_email) {
-			$request_parameters['SUBJECT'] = $this->merchant_email;
-		}
-		$request_parameters = array_merge($request_parameters,$nvp_parameters);
-
-		// Get response from the server.
-		$http_response = CASHSystem::getURLContents($this->api_endpoint,$request_parameters,true);
-		if ($http_response) {
-			// Extract the response details.
-			$http_response = explode("&", $http_response);
-			$parsed_response = array();
-			foreach ($http_response as $i => $value) {
-				$tmpAr = explode("=", $value);
-				if(sizeof($tmpAr) > 1) {
-					$parsed_response[$tmpAr[0]] = urldecode($tmpAr[1]);
-				}
-			}
-
-			if((0 == sizeof($parsed_response)) || !array_key_exists('ACK', $parsed_response)) {
-				$this->setErrorMessage("Invalid HTTP Response for POST (" . $nvpreq . ") to " . $this->api_endpoint);
-				return false;
-			}
-
-			if("SUCCESS" == strtoupper($parsed_response["ACK"]) || "SUCCESSWITHWARNING" == strtoupper($parsed_response["ACK"])) {
-				return $parsed_response;
-			} else {
-				$this->setErrorMessage(print_r($parsed_response, true));
-				return false;
-			}
-		} else {
-			$this->setErrorMessage('could not reach Paypal servers');
+	public static function getUserInfo($credentials) {
+		require_once(CASH_PLATFORM_ROOT.'/lib/stripe/Stripe.php');
+		Stripe::setApiKey($credentials);
+		$user_info = Stripe_Account::retrieve();
+		//error_log("****USERINFO***".$user_info['email']);
+		return $user_info;
+	}
+	
+	/**
+	 * Exchange an authorization code for OAuth 2.0 credentials.
+	 *
+	 * @param String $authorization_code Authorization code to exchange for OAuth 2.0 credentials.
+	 * @return String Json representation of the OAuth 2.0 credentials.
+	 */
+	public static function exchangeCode($authorization_code,$client_id,$client_secret) {
+		require_once(CASH_PLATFORM_ROOT.'/lib/stripe/StripeOAuth.class.php');
+		try {
+			$client = new StripeOAuth($client_id, $client_secret);
+			return $client->getTokens($authorization_code);
+		} catch (Exception $e) {
 			return false;
 		}
 	}
 	
-	public function setExpressCheckout(
-		$payment_amount,
-		$ordersku,
-		$ordername,
-		$return_url,
-		$cancel_url,
-		$request_shipping_info=true,
-		$allow_note=false,
-		$currency_id='USD', /* 'USD', 'GBP', 'EUR', 'JPY', 'CAD', 'AUD' */
-		$payment_type='Sale', /* 'Sale', 'Order', or 'Authorization' */
-		$invoice=false
-	) {
-		// Set NVP variables:
-		$nvp_parameters = array(
-			'PAYMENTREQUEST_0_AMT' => $payment_amount,
-			'PAYMENTREQUEST_0_PAYMENTACTION' => $payment_type,
-			'PAYMENTREQUEST_0_CURRENCYCODE' => $currency_id,
-			'PAYMENTREQUEST_0_ALLOWEDPAYMENTMETHOD' => 'InstantPaymentOnly',
-			'PAYMENTREQUEST_0_DESC' => $ordername,
-			'RETURNURL' => $return_url,
-			'CANCELURL' => $cancel_url,
-			'L_PAYMENTREQUEST_0_AMT0' => $payment_amount,
-			'L_PAYMENTREQUEST_0_NUMBER0' => $ordersku,
-			'L_PAYMENTREQUEST_0_NAME0' => $ordername,
-			'NOSHIPPING' => '0',
-			'ALLOWNOTE' => '0',
-			'SOLUTIONTYPE' => 'Sole',
-			'LANDINGPAGE' => 'Billing'
-		);
-		if (!$request_shipping_info) {
-			$nvp_parameters['NOSHIPPING'] = 1;
-		}
-		if ($allow_note) {
-			$nvp_parameters['ALLOWNOTE'] = 1;
-		}
-		if ($invoice) {
-			$nvp_parameters['PAYMENTREQUEST_0_INVNUM'] = $invoice;
-		}
-		
-		$parsed_response = $this->postToPaypal('SetExpressCheckout', $nvp_parameters);
-		if (!$parsed_response) {
-			$this->setErrorMessage('SetExpressCheckout failed: ' . $this->getErrorMessage());
-			return false;
-		} else {
-			// Redirect to paypal.com.
-			$token = urldecode($parsed_response["TOKEN"]);
-			$paypal_url = $this->paypal_base_url . "_express-checkout&token=$token";
-			return $paypal_url;
-		}
+	public static function getAuthorizationUrl($client_id,$client_secret) {
+		require_once(CASH_PLATFORM_ROOT.'/lib/stripe/StripeOAuth.class.php');
+		$client = new StripeOAuth($client_id, $client_secret);
+		$auth_url = $client->getAuthorizeUri();
+		return $auth_url;	
 	}
-	
-	public function getExpressCheckout() {
-		if ($this->token) {
-			$nvp_parameters = array(
-				'TOKEN' => $this->token
-			);
-			$parsed_response = $this->postToPaypal('GetExpressCheckoutDetails', $nvp_parameters);
-			if (!$parsed_response) {
-				$this->setErrorMessage('GetExpressCheckoutDetails failed: ' . $this->getErrorMessage());
-				return false;
-			} else {
-				return $parsed_response;
-			}
-		} else {
-			$this->setErrorMessage("No token was found.");
-			return false;
-		}
-	}
-	
-	public function doExpressCheckout($payment_type='Sale') {
-		if ($this->token) {
-			$token_details = $this->getExpressCheckout();
-			$nvp_parameters = array(
-				'TOKEN' => $this->token,
-				'PAYERID' => $token_details['PAYERID'],
-				'PAYMENTREQUEST_0_PAYMENTACTION' => $payment_type,
-				'PAYMENTREQUEST_0_AMT' => $token_details['PAYMENTREQUEST_0_AMT'],
-				'PAYMENTREQUEST_0_CURRENCYCODE' => $token_details['PAYMENTREQUEST_0_CURRENCYCODE'],
-				'PAYMENTREQUEST_0_ALLOWEDPAYMENTMETHOD' => 'InstantPaymentOnly'
-			);
-			
-			$parsed_response = $this->postToPaypal('DoExpressCheckoutPayment', $nvp_parameters);
-			if (!$parsed_response) {
-				$this->setErrorMessage($this->getErrorMessage());
-				return false;
-			} else {
-				return $parsed_response;
-			}
-		} else {
-			$this->setErrorMessage("No token was found.");
-			return false;
-		}
-	}
-	
-	public function doRefund($transaction_id, $refund_amount, $fullrefund=true,$memo=false,$currency_id='USD') {
-		if ($fullrefund) {
-			$refund_type = "Full";
-		} else {
-			$refund_type = "Partial";
-		}
-		
-		$nvp_parameters = array (
-			'TRANSACTIONID' => $transaction_id,
-			'REFUNDTYPE' => $refund_type,
-			'CURRENCYCODE' => $currency_id
-		);
 
-		if($memo) {
-			$nvp_parameters['NOTE'] = $memo;
-		}
-		
-		if (!$fullrefund) {
-			if(!isset($refund_amount)) {
-				$this->setErrorMessage('Partial Refund: must specify amount.');
-				return false;
-			} else {
-				$nvp_parameters['AMT'] = $refund_amount;
-			}
+	
 
-			if(!$memo) {
-				$this->setErrorMessage('Partial Refund: must specify memo.');
-				return false;
-			}
-		}
-		
-		$parsed_response = $this->postToPaypal('RefundTransaction', $nvp_parameters);
+	
 
-		if (!$parsed_response) {
-			$this->setErrorMessage('RefundTransaction failed: ' . $this->getErrorMessage());
-			return false;
-		}
-	}
 } // END class 
 ?>
